faf31d6420889b7c1967e707152f9166e924955f
CodingFeature <121998353+MovieGoers@users.noreply.github.com>
2023-01-12 16:58:57 +0900

    영화 평가 요소 추가.
    작품성, 연기, 오락성, 스토리 요소 추가. 평균 내는 시스템.

--- script.js
+++ script.js
@@ -1,42 +1,74 @@
 const movieNameText = document.createElement('h1');
 const movieName = document.createElement('h2');
-const whatsYourRating = document.createElement('h1');
-const movieRating = document.createElement('select');
+
+
 
 const averageRatingText = document.createElement('h1');
 const averageRating = document.createElement('h2');
 
-const ratingValues = ['onestar', 'twostar', 'threestar', 'fourstar', 'fivestar'];
-const ratingStarts = ['★', '★★', '★★★', '★★★★', '★★★★★']
 
-let i = 0;
+const movieRatingSelections = ['작품성', '연기', '오락성','스토리'];
+const ratingValues = [1, 2, 3, 4, 5];
+const ratingStars = ['★', '★★', '★★★', '★★★★', '★★★★★']
 
-for (ratevalue in ratingValues){
-    let movieRatingOption = document.createElement('option');
-    movieRatingOption.setAttribute('value', ratingValues[ratevalue]);
-    movieRatingOption.textContent = ratingStarts[i];
-    movieRating.appendChild(movieRatingOption);
-    i++;
+
+
+function ShowRatingOptions(){
+    for (select in movieRatingSelections){
+        let ratingText = document.createElement('h1');
+        let movieRatingSelection = document.createElement('select');
+        let movieRatingDiv = document.createElement('div');
+
+        movieRatingDiv.setAttribute("class", "movie-rating-div");
+        movieRatingSelection.setAttribute("id", movieRatingSelections[select]);
+        movieRatingSelection.setAttribute("onchange", "onChangeRating()");
+
+        ratingText.textContent = movieRatingSelections[select];
+        
+        let i = 0;
+        for (ratevalue in ratingValues){
+            let movieRatingOption = document.createElement('option');
+            movieRatingOption.setAttribute('value', ratingValues[ratevalue]);
+            movieRatingOption.textContent = ratingStars[i];
+            movieRatingSelection.appendChild(movieRatingOption);
+            i++;
+        }
+        
+        movieRatingDiv.appendChild(ratingText);
+        movieRatingDiv.appendChild(movieRatingSelection);
+
+        document.body.appendChild(movieRatingDiv);
+    }
 }
 
-movieNameText.textContent = "영화 제목";
-movieName.textContent = "아바타 2 : 물의 길";
-whatsYourRating.textContent = "당신의 평점은?";
-averageRatingText.textContent = "평균 점수?";
-
-movieRating.setAttribute('title', 'movieRaing');
-movieRating.setAttribute('onchange', 'onChange()');
-
-document.body.appendChild(movieNameText);
-document.body.appendChild(movieName);
-document.body.appendChild(whatsYourRating);
-document.body.appendChild(movieRating);
-document.body.appendChild(averageRatingText);
-document.body.appendChild(averageRating);
-
-
-
-function onChange(){
-    averageRating.textContent = movieRating.options[movieRating.selectedIndex].value;
-    
+function onChangeRating(){
+    averageRating.textContent = CalculateAverageRating() + '/' + ratingStars.length;
 }
+
+function CalculateAverageRating(){
+    let averageRateScore = 0;
+
+    for(movieRates in movieRatingSelections){
+        let tempRating = document.getElementById(movieRatingSelections[movieRates]);
+        averageRateScore += Number(tempRating.options[tempRating.selectedIndex].value);
+        
+    }
+    return averageRateScore / movieRatingSelections.length;
+}
+
+function ShowAverageRating(){
+    averageRatingText.textContent = "평균 점수?";
+    document.body.appendChild(averageRatingText);
+    document.body.appendChild(averageRating);
+}
+
+function ShowMovieName(){
+    movieNameText.textContent = "영화 제목";
+    movieName.textContent = "아바타 2 : 물의 길";
+    document.body.appendChild(movieNameText);
+    document.body.appendChild(movieName);
+}
+
+ShowMovieName();
+ShowRatingOptions();
+ShowAverageRating();
